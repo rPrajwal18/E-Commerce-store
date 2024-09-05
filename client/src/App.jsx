3bbd48905dfb967f72245b9bd7a1ee302d42257a
rPrajwal18 <18rprajwal@gmail.com>
2024-09-05 16:40:16 +0530

    added footer 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,8 +19,8 @@ function App() {
           </Routes>
         </div>
       </Router>
-      <footer className="footer">
-                Developed by Prajwal R 🚀
+      <footer className="footer bg-primary text-white text-center py-4">
+        Developed by Prajwal R
       </footer>
     </div>
   );
